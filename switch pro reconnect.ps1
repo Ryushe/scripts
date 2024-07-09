a1c470dd67944cf8aab2c56d9affd9e87464bdac
Ryushe <ryushe.dev@gmail.com>
2024-07-09 14:54:07 -0700

    betterjoy x butter pro script

--- switch pro reconnect.ps1
+++ switch pro reconnect.ps1
@@ -1,47 +1,58 @@
 
-$device = Get-PnpDevice -class Bluetooth -FriendlyName "Lic Pro Controller"
+$device = Get-PnpDevice -class Bluetooth -FriendlyName "Lic Pro Controller" -ErrorAction SilentlyContinue
 $command = Get-Command "btpair" -ErrorAction SilentlyContinue
+$isBetterJoyRunning = Get-Process -Name "BetterJoyForCemu" -ErrorAction SilentlyContinue
 
+
+Function BetterJoyChecker() {
+    Write-Host "Checking for betterjoy..."
+    if($isBetterJoyRunning) {
+        Write-Host "BetterJoy is already Running"
+        Start-Sleep 5
+        exit
+    } Else {
+        Write-Host "Starting BetterJoy"
+        Start-Process ".\dependencies\BetterJoy\BetterJoyForCemu.exe"
+    }
+}
+
+Function PairDevice() {
+    Write-Host "Starting Pairing... (hold the sync button now)"
+    btpair -p -n"Lic Pro Controller"
+    if($device) {
+        Write-Host "Device Paired"
+    } Else {
+        Write-Host "Device Errored, Rerun Script"
+    }
+}
 Function FuckNintendo {
-    Do {
-        If ($command) { # does btpair exist
-            If($device) {
-                Write-Host "Removing Device"
-                btpair -u -n"Lic Pro Controller"
-                Write-Host "Device Removed Sucessfully"
-                Write-Host "Starting Pairing... (hold the sync button now)"
-                btpair -p -n"Lic Pro Controller"
-                if($device) {
-                    Write-Host "Device Paired"
-                } Else {
-                    Write-Host "Device Not Paired"
-                }
-            } Else {
-                Write-Host "No Device Named Lic Pro Controller"
-                Write-Host "Starting Pairing..."
-                btpair -p -n"Lic Pro Controller"
-                if($device) {
-                    Write-Host "Device Paired"
-                } Else {
-                    Write-Host "Device Not Paired"
-                }
-            }
+    If ($command) { # does btpair exist
+        If($device) {
+            Write-Host "Removing Device"
+            btpair -u -n"Lic Pro Controller"
+            @(PairDevice)
         } Else {
+            Write-Host "No Device Named Lic Pro Controller"
+            @(PairDevice)
+        }
+    } Else {
 Write-Host @'
 LMAO btpair not on system
 Opening link for you bbg
 
 Then Rerun script
 '@
-            Start-Sleep 5
-            Start-Process "https://bluetoothinstaller.com/bluetooth-command-line-tools"
-            exit 
-        }
-    } while (True) 
+        Start-Sleep 5
+        Start-Process "https://bluetoothinstaller.com/bluetooth-command-line-tools"
+        exit 
+    }
 
+    @(BetterJoyChecker)
 }
 @(FuckNintendo)
-# $Source = @"
+
+    
+
 #    [DllImport("BluetoothAPIs.dll", SetLastError = true, CallingConvention = CallingConvention.StdCall)]
 #    [return: MarshalAs(UnmanagedType.U4)]
 #    static extern UInt32 BluetoothRemoveDevice(IntPtr pAddress);
